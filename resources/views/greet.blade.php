a1650674134209c502481e9e313ab51bf06ce7c0
Luis Gimpaya <113194420+Licht2003@users.noreply.github.com>
2025-02-15 13:21:11 +0000

    New Activity

--- resources/views/greet.blade.php
+++ resources/views/greet.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Laravel Greeting</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/greet.csss">
+    <title>Greet</title>
 </head>
 <body>
-    <h1>Hello, Laravel!</h1>
+    <h2>Hello, Welcome to Mastery in Web Technologies</h2>
+    <p>This is a paragraph</p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\GreetController;
 
 Route::get('/', function () {
     return view('welcome');
 });
- 
-Route::get('/', function () {
-    return 'Hello, Laravel!'; 
+
+Route::get('/hello', function () {
+    return 'Hello, Laravel';
 });
 
-Route::get('/greeting', [GreetController::class, 'showGreeting']);
+Route::get('/greetings',[GreetController::class, 'showGreetings']);
+
+Route::resource('tasks', TaskController::class);
